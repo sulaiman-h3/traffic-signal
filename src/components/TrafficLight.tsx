20e0f8f8f42fc585175c0e56b3f013ab6dd560df
sulaiman sulaiman <sulaiman.sulaiman@evolvextechnology.com>
2026-08-10 21:35:08 +0400

    Initial commit

---
+++ src/components/TrafficLight.tsx
@@ -0,0 +1,13 @@
+const TrafficLight = () => {
+  return (
+    <div className="traffic-light-container">
+  <div className="traffic-light">
+    <div className="light red"></div>
+    <div className="light yellow"></div>
+    <div className="light green"></div>
+  </div>
+</div>
+  );
+};
+
+export default TrafficLight;
