6b122f01b2bd0ee0609c495e460e314008bc3999
sulaiman sulaiman <sulaiman.sulaiman@evolvextechnology.com>
2026-08-11 12:53:37 +0400

    colors change

--- src/components/TrafficLight.tsx
+++ src/components/TrafficLight.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
+
 const TrafficLight = () => {
+  const [lights, setLights] = useState(["red", "yellow", "green"]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLights((prevLights) => {
+        return [
+          prevLights[2],
+          prevLights[0],
+          prevLights[1],
+        ];
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="traffic-light-container">
-  <div className="traffic-light">
-    <div className="light red"></div>
-    <div className="light yellow"></div>
-    <div className="light green"></div>
-  </div>
-</div>
+      <div className="traffic-light">
+        <div className={`light ${lights[0]}`}></div>
+        <div className={`light ${lights[1]}`}></div>
+        <div className={`light ${lights[2]}`}></div>
+      </div>
+    </div>
   );
 };
 
