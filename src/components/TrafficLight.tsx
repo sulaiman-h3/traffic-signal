16b5f2a54a14c04c264a28b1505d164cb596798a
sulaiman sulaiman <sulaiman.sulaiman@evolvextechnology.com>
2026-08-11 21:47:59 +0400

    obacity

--- src/components/TrafficLight.tsx
+++ src/components/TrafficLight.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const TrafficLight = () => {
   const [lights, setLights] = useState(["red", "yellow", "green"]);
+  const [opacityLights, setOpacityLights] = useState(["red", "yellow", "green"]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -18,12 +19,41 @@ const TrafficLight = () => {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOpacityLights((prevLights) => {
+        const newLights = [...prevLights];
+
+        const firstLight = newLights.shift();
+        newLights.push(firstLight);
+
+        return newLights;
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="traffic-light-container">
       <div className="traffic-light">
-        <div className={`light ${lights[0]}`}></div>
-        <div className={`light ${lights[1]}`}></div>
-        <div className={`light ${lights[2]}`}></div>
+        <div
+          className={`light ${lights[0]} ${
+            lights[0] === opacityLights[0] ? "active" : ""
+          }`}
+        ></div>
+
+        <div
+          className={`light ${lights[1]} ${
+            lights[1] === opacityLights[0] ? "active" : ""
+          }`}
+        ></div>
+
+        <div
+          className={`light ${lights[2]} ${
+            lights[2] === opacityLights[0] ? "active" : ""
+          }`}
+        ></div>
       </div>
     </div>
   );
