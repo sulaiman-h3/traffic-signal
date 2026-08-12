7aeaa2fe154f1757492ce54e32b163574866bbab
sulaiman sulaiman <sulaiman.sulaiman@evolvextechnology.com>
2026-08-12 15:18:03 +0400

    Modal

--- src/components/newmodal.tsx
+++ src/components/newmodal.tsx
@@ -3,25 +3,32 @@ import { useState } from "react";
 const NewModal = () => {
   const [modal, setModal] = useState(false);
 
-  function openModal() {
-    setModal(true);
-  }
-
-  function closeModal() {
-    setModal(false);
+  function taggle() {
+    setModal(!modal);
   }
 
   return (
     <>
-      <button onClick={openModal}>Open a modal</button>
-
-      {modal && (
-        <div className="modal">
-          <h2>Done</h2>
-          <p>What's the next step?</p>
-          <button onClick={closeModal}>Close</button>
+     <button onClick={taggle} className="btn-modal">
+        Open a modal
+      </button>
+    {modal && (<div className="modal">
+        <div className="overlay">
+          <div className="modal-content">
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio aut et minus fuga aspernatur soluta asperiores odit
+              illo eaque excepturi. Dolor officia a excepturi dolorem et
+              eveniet, harum qui inventore!
+            </p>
+            <button onClick={taggle} className="close-modal">close modal</button>
+          </div>
         </div>
-      )}
+      </div>)}
+     
+
+      
     </>
   );
 };
